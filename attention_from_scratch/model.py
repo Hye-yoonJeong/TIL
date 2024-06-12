ae4f2c768324adf098ca612a51f2104051714781
Hye-yoonJeong <hyj89han@gmail.com>
2024-06-12 02:22:51 +0000

    initial commit

---
+++ attention_from_scratch/model.py
@@ -0,0 +1,65 @@
+# input embedding
+
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model : int, vocab_size : int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        
+    def forward(self, x):
+        return self.embedding*math.sqrt(self.d_model)
+        
+
+# positional encoding
+class PositionalEmbedding(nn.Module):
+    def __init__(self, d_model:int, seq_len:int, dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = dropout
+        
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        
+        # Apply the sin to even position
+        pe[:, 0::2] = torch.sin(position*div_term)
+        pe[:, 1::2] = torch.cos(position*div_term)
+        
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+        
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shapep[1], :]).requires_grad_(False)
+        return self.dropout(x)
+    
+# Layer Normalization
+class LayerNormalization(nn.Module):
+    def __init__(self, eps:float= 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) # Multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) # Added
+        
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim=True)
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * (x-mean) / (std+self.eps) + self.bias
+
+# Feed Forward - fully connected layer
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model: int, d_ff:int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+
+        
